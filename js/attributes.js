707dd00c1722c86afa5e9802c9d5ff3632139fbe
dienast <byronschuurman@live.nl>
2013-06-10 14:28:19 +0300

    Update attributes.js

--- js/attributes.js
+++ js/attributes.js
@@ -6,22 +6,27 @@ jQuery(document).ready(function($) {
 	
 	/* Show or hide fields when type is changed */
 	$('.field_type').change(function() {
+		
 		switch($(this).val()) {
 			case 'text':
 				$(this).parent().parent().prev().hide();
 				$(this).parent().parent().next().find('.field_required').show();	
+				$(this).parent().parent().next().find('.field_range').hide();	
 				break;
 			case 'select':	
 				$(this).parent().parent().prev().show();
-				$(this).parent().parent().next().find('.field_required').show();
+				$(this).parent().parent().next().next().find('.field_required').show();
+				$(this).parent().parent().next().next().find('.field_range').show();	
 				break;
 			case 'radio':	
 				$(this).parent().parent().prev().show();
 				$(this).parent().parent().next().find('.field_required').show();
+				$(this).parent().parent().next().find('.field_range').show();	
 				break;					
 			case 'checkbox': 
 				$(this).parent().parent().prev().hide();
 				$(this).parent().parent().next().find('.field_required').hide();
+				$(this).parent().parent().next().find('.field_range').hide();	
 				break;
 		}
 	});
